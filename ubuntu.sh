6db1b53108da8b590701f43b4fdf225fb14e72a2
Phùng Quốc Việt <92231102+vietpq685@users.noreply.github.com>
2024-11-20 21:47:51 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -49,7 +49,6 @@ sudo chmod a+wx config.jsonc
 cd ~
 
 echo "Config Oh-My-Posh"
-sudo apt install wget -y
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 
