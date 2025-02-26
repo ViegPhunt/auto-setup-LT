98c61cb41dcfea3be4cecb4d71e1cbffb3162f96
Phùng Quốc Việt <92231102+vietpq685@users.noreply.github.com>
2025-02-27 00:16:27 +0700

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -8,7 +8,7 @@ sudo dnf update -y
 cd ~
 
 echo "Download some terminal tool"
-sudo dnf install -y perl-Image-ExifTool make tmux btop cmatrix cbonsai cowsay unzip curl wget dpkg fzf eza bat zoxide neovim python3 nodejs npm ripgrep stow
+sudo dnf install -y perl-Image-ExifTool make tmux btop cmatrix cbonsai cowsay unzip curl wget dpkg fzf eza bat zoxide neovim python3 nodejs npm ripgrep stow python3-pwntools
 git clone https://github.com/pipeseroni/pipes.sh.git
 cd pipes.sh
 sudo make install
