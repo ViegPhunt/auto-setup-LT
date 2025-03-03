fbdced8fb50164425dbec2ac1c104df4fa4b779e
Phùng Quốc Việt <92231102+vietpq685@users.noreply.github.com>
2025-03-03 18:09:05 +0700

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -62,7 +62,7 @@ sudo pacman -S zsh --noconfirm
 
 echo "Download file config"
 git clone https://github.com/vietpq685/auto-setup-LT.git ~/setup
-mv ~/setup/dotfiles ~/dotfiles
+mkdir -p ~/dotfiles && mv ~/setup/dotfiles/* ~/dotfiles/
 rm -rf setup
 mv ~/dotfiles/fastfetch/.config/fastfetch/arch.jsonc ~/dotfiles/fastfetch/.config/fastfetch/config.jsonc
 git clone https://github.com/tmux-plugins/tpm ~/dotfiles/tmux/.tmux/plugins/tpm
