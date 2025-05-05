e3c57a4cb366ca942fc86fdf091874e6e8a2d6d1
Phùng Quốc Việt <92231102+ViegPhunt@users.noreply.github.com>
2025-05-05 22:30:56 +0700

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-echo "WELCOME! Now we will customize Arch Terminal"
-echo "Created by Phunt_Vieg_"
+clear
 
-sudo pacman -Syu --noconfirm
+echo "==> WELCOME! Now we will customize Arch Terminal"
+echo "==> Created by Phunt_Vieg_"
 
 cd ~
 
-echo 'Setting locale'
+echo "==> Updating system packages..."
+sudo pacman -Syu --noconfirm
+
+echo '==> Setting locale'
 echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
 sudo locale-gen
 echo "LANG=en_US.UTF-8" | sudo tee -a /etc/locale.conf
 export LANG=en_US.UTF-8
 
-echo "Download some terminal tool"
+echo "==> Download some terminal tool"
 sudo pacman -S --noconfirm --needed base-devel git
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -32,10 +35,10 @@ sudo make install
 cd ..
 rm -rf pipes.sh
 
-echo "Allow pip3 install by removing EXTERNALLY-MANAGED file"
+echo "==> Allow pip3 install by removing EXTERNALLY-MANAGED file"
 sudo rm -rf $(python3 -c "import sys; print(f'/usr/lib/python{sys.version_info.major}.{sys.version_info.minor}/EXTERNALLY-MANAGED')")
 
-echo "Download pwndbg and pwntools"
+echo "==> Download pwndbg and pwntools"
 sudo pacman -S gdb --noconfirm
 git clone --depth 1 https://github.com/pwndbg/pwndbg
 cd pwndbg
@@ -43,33 +46,33 @@ cd pwndbg
 cd ..
 pip3 install pwntools
 
-echo "Download pokemon-colorscripts"
+echo "==> Download pokemon-colorscripts"
 git clone --depth 1 https://gitlab.com/phoneybadger/pokemon-colorscripts.git
 cd pokemon-colorscripts
 sudo ./install.sh
 cd ..
 rm -rf ~/pokemon-colorscripts
 
-echo "Config Oh-My-Posh"
+echo "==> Config Oh-My-Posh"
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 
-echo "Download Fastfetch"
+echo "==> Download Fastfetch"
 sudo pacman -S fastfetch --noconfirm
 
-echo "Config Zsh"
+echo "==> Config Zsh"
 sudo pacman -S zsh --noconfirm
 
-echo "Download file config"
+echo "==> Download file config"
 git clone --depth 1 https://github.com/ViegPhunt/Dotfiles.git ~/dotfiles
 mv ~/dotfiles/.config/fastfetch/arch.jsonc ~/dotfiles/.config/fastfetch/config.jsonc
 git clone --depth 1 https://github.com/tmux-plugins/tpm ~/dotfiles/.tmux/plugins/tpm
 
-echo "Stow"
+echo "==> Stow"
 cd ~/dotfiles
 rm -rf .git README.md
 stow -t ~ .
 cd ~
 
-echo 'Change shell'
+echo '==> Change shell'
 chsh -s /usr/bin/zsh
