2dccf3b695748dd722dda3a7852d0822af570c0d
Viet Phung <phungquocviet0608@gmail.com>
2025-07-20 16:42:01 +0700

    Add Update script

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Updating config..."
+
+rm -rf ~/dotfiles
+
+git clone --depth=1 -q https://github.com/ViegPhunt/Dotfiles.git ~/dotfiles
+git clone --depth=1 -q https://github.com/tmux-plugins/tpm ~/dotfiles/.tmux/plugins/tpm
+
+cd ~/dotfiles
+rm -rf .git README.md
+
+echo
+echo "==> Config update completed!"
+echo
