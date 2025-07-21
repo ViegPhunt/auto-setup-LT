81c505a13f7fe9fb90b76fd5b69bb1ddfd3c9590
Phùng Quốc Việt <92231102+ViegPhunt@users.noreply.github.com>
2025-07-22 02:22:38 +0700

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -62,7 +62,7 @@ git clone --depth=1 https://github.com/tmux-plugins/tpm ~/dotfiles/.tmux/plugins
 
 echo "==> Stow"
 cd ~/dotfiles
-rm -rf .git README.md
+rm -rf .git README.md LICENSE
 stow -t ~ .
 cd ~
 
